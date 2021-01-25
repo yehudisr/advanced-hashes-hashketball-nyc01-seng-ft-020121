886253c763a09b515d2126411deb32a366110d31
yehudis raitman <yehudisr@gmail.com>
2021-01-25 18:01:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -139,8 +139,8 @@ def num_points_scored(name)
   game_hash.each do |key, value|
     value.each do |inner_key, inner_value|
       if inner_key == :players
-      inner_value.each do |player|
-       if player[:player_name] == name
+      inner_value.find do |player|
+       player[:player_name] == name
     binding.pry  
     #binding.pry
         end
