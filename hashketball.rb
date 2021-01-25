65916c29a864c1125907c0949cf80ea66a481e9a
yehudis raitman <yehudisr@gmail.com>
2021-01-25 19:00:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -176,7 +176,7 @@ def num_points_scored(name)
 def team_colors (team)
 game_hash.each do |key, value|
   value.each do |inner_key, inner_value|
-    if inner_key == :colors
+    if team == value  && inner_key == :colors
       return inner_value
       binding.pry
     end 
